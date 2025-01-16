522b876d87636d0c1b12030ad38e93c551057e66
rezokip <ozerkip@hotmail.de>
2025-01-16 08:46:08 +0100

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-
+import WorkSessionComponent from './components/WorkSessionComponent'
+import EmployeeSelection from './components/EmployeeSelection'
+import LoginComponent from './components/LoginComponent'
 
 function App() {
 
@@ -9,6 +11,10 @@ function App() {
     <BrowserRouter>
       <Routes>
         
+        <Route path='/' element = {<LoginComponent/>}></Route>
+        <Route path = '/erfassung/arbeitszeiten' element={<EmployeeSelection/>}></Route>
+        <Route path = '/work-session/:employeeName' element={<WorkSessionComponent/>}></Route>
+
 
       </Routes>
     </BrowserRouter>
