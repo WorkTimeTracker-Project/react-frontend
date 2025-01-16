c836423f2a1e8144f9c63227a78366fb69640ddf
rezokip <ozerkip@hotmail.de>
2025-01-16 08:55:50 +0100

    updatet routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
+import ListWorkSession from './components/ListWorkSession'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import WorkSessionComponent from './components/WorkSessionComponent'
 import EmployeeSelection from './components/EmployeeSelection'
 import LoginComponent from './components/LoginComponent'
 import AdminUIComponent from './components/AdminUIComponent'
+import AddEmployee from './components/AddEmployee'
+import DeleteEmployee from './components/DeleteEmployee'
 import ProtectedRoute from './components/ProtectedRoute'; 
 
 function App() {
@@ -15,6 +18,10 @@ function App() {
         
         <Route path='/' element = {<LoginComponent/>}></Route>
         <Route path='/admin' element={<AdminUIComponent />} />
+        <Route path='/admin-dashboard' element={<ProtectedRoute element={<ListWorkSession />} isAdmin={true} />} />
+        <Route path='/employee/add' element={<ProtectedRoute element={<AddEmployee />} isAdmin={true} />} />
+        <Route path='/employee/delete' element={<ProtectedRoute element={<DeleteEmployee />} isAdmin={true} />} />
+
         <Route path='/erfassung/arbeitszeiten' element={<ProtectedRoute element={<EmployeeSelection />} isAdmin={false} />} />
         <Route path='/work-session/:employeeName' element={<ProtectedRoute element={<WorkSessionComponent />} isAdmin={false} />} />
  
