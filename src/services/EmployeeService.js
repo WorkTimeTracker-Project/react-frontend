8c73ee0ffc53713a0abaad52bdba213300e4ec20
rezokip <ozerkip@hotmail.de>
2025-01-17 16:22:43 +0100

    added env variable for rest api

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://192.168.2.174:8080/api/work-sessions/employee'; 
+const apiUrl = process.env.REACT_APP_API_URL
+const API_BASE_URL = `${apiUrl}/api/work-sessions/employee`; 
 
 export const getAllEmployees = async () => {
   const response = await axios.get(API_BASE_URL);
